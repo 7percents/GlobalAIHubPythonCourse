083c05e071a6dfa5747484bd6fe84e948b682618
7percents <61332914+7percents@users.noreply.github.com>
2021-03-09 22:05:26 +0300

    Update hw1.py

--- Homeworks/hw1.py
+++ Homeworks/hw1.py
@@ -1,9 +1,10 @@
 # Explain your work
 '''
 I created two lists and then merged them.
-For clarity, I also used sort to sort merged list.
+For clarity, I also used the sort method to sort the merged list.
 Lastly, I used a for loop to print the type of each element in the final list.
 '''
+
 # Answer to Question 1
 even_num = list(range(0, 10, 2))
 odd_num = list(range(1, 10, 2))
